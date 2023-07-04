00c33c5d078fb6e81e45ee470ce51a1b4cb354da
Rishabh Dandriyal <rishabh.dandriyal@nagarro.com>
2023-07-04 15:01:20 +0530

    requirements added and installation done

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+_version = "0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "RishabhDandriyal03"
+SRC_REPO = "TextSummarzer"
+AUTHOR_EMAIL = "rishudandriyal@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=_version,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
